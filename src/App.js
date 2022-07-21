74269c602078d87ec408408d9a8cb855c57433d2
Jlabe <72110585+Jlabe@users.noreply.github.com>
2022-07-21 06:05:00 -0400

    Change route names

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Navbar from './Navbar';
 import Home from './Home';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch, useHistory} from 'react-router-dom';
 import LeagueStats from './LeagueStats';
 import Table from './Table';
 import TeamStats from './TeamStats';
